9fc304244debcdb2779f3f8bcd77bc348e59b881
itjack <itjack@qq.com>
2023-02-07 00:13:01 +0800

    初始化插件化

---
+++ installer/config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'require-dev' => [],
+    'questions' => [],
+    'directory' => [
+        'Controller', 'Model', 'Service', 'Event', 'Constants', 'Contract', 'Job', 'Queue', 'Task', 'Dao'
+    ]
+];
