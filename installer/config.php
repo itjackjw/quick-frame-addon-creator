86c7519d349abe9d59be142d7200b38e73a44bca
itjack <1092428238@qq.com>
2023-02-09 13:08:59 +0800

    1、新增表单验证

--- installer/config.php
+++ installer/config.php
@@ -4,6 +4,6 @@ return [
     'require-dev' => [],
     'questions' => [],
     'directory' => [
-        'Controller','Command' ,'Model', 'Service', 'Event', 'Constants', 'Contract', 'Job', 'Queue', 'Task', 'Dao'
+        'Controller','Request','Command' ,'Model', 'Service', 'Event', 'Constants', 'Contract', 'Job', 'Queue', 'Task', 'Dao'
     ]
 ];
